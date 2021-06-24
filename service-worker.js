df26c83faec894ee6604b2e72de23ccdac76d107
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-06-24 17:47:39 -0400

    added manifest

--- service-worker.js
+++ service-worker.js
@@ -44,7 +44,7 @@ self.addEventListener('activate', function(e) {
     );
   });
   self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
+    //console.log('fetch request : ' + e.request.url)
     e.respondWith(
       caches.match(e.request).then(function (request) {
         if (request) { // if cache is available, respond with cache
